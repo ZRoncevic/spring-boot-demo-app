0901f958d190fa36c4ca27960af72260a65dae74
ZRoncevic <zroncevi@gmail.com>
2017-01-17 11:37:24 +0100

    Update MethodLoggerAop.java

--- src/main/java/com/zroncevic/aop/MethodLoggerAop.java
+++ src/main/java/com/zroncevic/aop/MethodLoggerAop.java
@@ -15,7 +15,7 @@ public class MethodLoggerAop {
 	@ConditionalOnProperty(name = "method.logger.enabled", matchIfMissing = false)
 	public class MethodLogger {
 
-	    @Before("execution(* com.zroncevic..*(..))")
+	    @Before("execution(* com.zroncevic..*.*(..))")
 	    public void before(JoinPoint point) throws Throwable {
 	        
 	    	System.out.println("AOP IN :" +
@@ -24,7 +24,7 @@ public class MethodLoggerAop {
 	        return;
 	    }
 
-	    @AfterReturning(pointcut = "execution(* com.zroncevic..*(..))", returning = "result")
+	    @AfterReturning(pointcut = "execution(* com.zroncevic..*.*(..))", returning = "result")
 	    public void after(JoinPoint point, Object result) throws Throwable {
 	        long start = System.currentTimeMillis();
 	        System.out.println(MethodSignature.class.cast(point.getSignature()).getMethod().getName()
